390272699fde4aa21c2806e1db9ec8d5d84c981b
zy tan <zarn@wright.com.sg>
2020-09-25 18:41:08 +0800

    Changed MarkupSafe version

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
 def evaluate():
-    data = request.get_json();
+    data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
+    inputValue = data.get("input")
     result = inputValue * inputValue
     logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return json.dumps(result)
 
 
 
