a98d0ed5bcb675b1cd3ea8f8d5b729d54be6736e
zy tan <zarn@wright.com.sg>
2020-09-27 01:17:39 +0800

    Update GMO.py

--- codeitsuisse/routes/GMO.py
+++ codeitsuisse/routes/GMO.py
@@ -23,10 +23,6 @@ def evaluateGMO():
         sorted_seq = []
         cc_count = 0
         acgt_count = 0
-        while charCount["C"] > 1:
-            sorted_seq.append("CC")
-            charCount["C"] -= 2
-            cc_count += 1
         while charCount["A"] > 0 and charCount["C"] > 0 and charCount["G"] > 0 and charCount["T"] > 0:
             sorted_seq.append("ACGT")
             charCount["A"] -= 1
@@ -34,20 +30,24 @@ def evaluateGMO():
             charCount["G"] -= 1
             charCount["T"] -= 1
             acgt_count += 1
+        while charCount["C"] > 1:
+            sorted_seq.append("CC")
+            charCount["C"] -= 2
+            cc_count += 1
         for k,v in charCount.items():
             if k != "A":
                 for i in range(v):
                     sorted_seq.append(k)
+        for count in range(acgt_count):
+            if charCount["A"] > 0:
+                sorted_seq.insert(cc_count*2 + count*2, "A")
+                charCount["A"] -= 1
         for count in range(cc_count):
             if charCount["A"] > 1:
                 sorted_seq.insert(count*2, "AA")
                 charCount["A"] -= 2
             elif charCount["A"] == 1:
                 sorted_seq.insert(count*2, "AA")
-                charCount["A"] -= 1
-        for count in range(acgt_count):
-            if charCount["A"] > 0:
-                sorted_seq.insert(cc_count*2 + count*2, "A")
                 charCount["A"] -= 1
         count = 0
         while charCount["A"] > 0:
