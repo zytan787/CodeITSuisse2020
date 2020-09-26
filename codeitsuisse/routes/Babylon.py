53fa4726f1ca920cd942f364c85f890e326b924b
zy tan <zarn@wright.com.sg>
2020-09-26 18:36:06 +0800

    Update Babylon.py

--- codeitsuisse/routes/Babylon.py
+++ codeitsuisse/routes/Babylon.py
@@ -7,31 +7,31 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-def maxBook(books, days, time_left, no_books, no_days, i, dp):  
+# def maxBook(books, days, time_left, no_books, no_days, i, dp):  
 
-    # Base case  
-    if i == no_books: 
-        return 0
+#     # Base case  
+#     if i == no_books: 
+#         return 0
 
-    temp = dp[i]
-    for k in range(no_days-1):
-        temp = temp[time_left[k]]
+#     temp = dp[i]
+#     for k in range(no_days-1):
+#         temp = temp[time_left[k]]
 
-    if temp[time_left[-1]] != -1:  
-        return temp[time_left[-1]]
+#     if temp[time_left[-1]] != -1:  
+#         return temp[time_left[-1]]
    
-    fills = [0 for t in range(no_days)]
-    fill_none = 0
+#     fills = [0 for t in range(no_days)]
+#     fill_none = 0
 
-    for k in range(no_days):
-        if time_left[k] >= books[i]:
-            fills[k] = 1 + maxBook(books, days, time_left[:k] + [time_left[k]-books[i]] + time_left[k+1:], no_books, no_days, i + 1, dp)  
+#     for k in range(no_days):
+#         if time_left[k] >= books[i]:
+#             fills[k] = 1 + maxBook(books, days, time_left[:k] + [time_left[k]-books[i]] + time_left[k+1:], no_books, no_days, i + 1, dp)  
   
-    fill_none = maxBook(books, days, time_left[:], no_books, no_days, i+1, dp)  
+#     fill_none = maxBook(books, days, time_left[:], no_books, no_days, i+1, dp)  
   
-    temp[time_left[-1]] = max(fill_none, max(fills))  
+#     temp[time_left[-1]] = max(fill_none, max(fills))  
       
-    return temp[time_left[-1]]
+#     return temp[time_left[-1]]
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
 def evaluateBabylon():
@@ -39,17 +39,38 @@ def evaluateBabylon():
     logging.info("data sent for evaluation {}".format(data))
     no_books = data.get("numberOfBooks")
     no_days = data.get("numberOfDays")
-    books = data.get("books")
+    ori_books = data.get("books")
     days = data.get("days")
 
-    dp = -1
-    for j in range(no_days):
-        dp = [dp for i in range(200)]
+    max_ans = -1
 
-    dp = [dp for i in range(200)]
+    lst = [i for i in range(no_days)]
+    big_lst = []
+    for i in range(len(lst)):
+        big_lst.append(lst[i:] + lst[:i])
 
-    # result = {"optimalNumberOfBooks": maxBook(books, days, days, no_books, no_days, 0, dp)}
-    result = {"optimalNumberOfBooks": 8}
+    for seq in big_lst:
+        books = ori_books[:]
+        ans = 0
+        for k in seq:
+            dp = [[[0,[]] for i in range(days[k]+1)] for j in range(len(books)+1)]
+
+            for i in range(1, len(books)+1):
+                for j in range(1, days[k]+1):
+                    if j >= books[i-1]:
+                        if (1 + dp[i-1][j-books[i-1]][0]) > dp[i-1][j][0]:
+                            dp[i][j] = [1 + dp[i-1][j-books[i-1]][0], dp[i-1][j-books[i-1]][1][:] + [i-1]]
+                        else:
+                            dp[i][j] = dp[i-1][j][:]
+
+            ans += dp[-1][-1][0]
+            dp[-1][-1][1].sort(reverse=True)
+            for to_drop in dp[-1][-1][1]:
+                books.pop(to_drop)
+
+        max_ans = max(max_ans, ans)
+
+    result = {"optimalNumberOfBooks": max_ans}
 
     logging.info("My result :{}".format(result))
     return jsonify(result)
