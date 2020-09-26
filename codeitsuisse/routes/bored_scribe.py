f11f6f5575b193bc7d280f83aebff4b89de9ccf2
zy tan <zarn@wright.com.sg>
2020-09-27 04:32:32 +0800

    Update bored_scribe.py

--- codeitsuisse/routes/bored_scribe.py
+++ codeitsuisse/routes/bored_scribe.py
@@ -1,5 +1,7 @@
 import logging
 import json
+import enchant
+d = enchant.Dict("en_US")
 
 from flask import request, jsonify;
 
@@ -8,45 +10,61 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 def decrypt(s):
-    palindromes = set()
-    for i in range(len(s)):
-        palindromes = expandAroundCenter(s, i, i, palindromes)
-        palindromes = expandAroundCenter(s, i, i+1, palindromes)
+    ans = s
+    real_ans = ""
+    encryption_count = 0
+    while not real_ans:
+        encryption_count += 1
+        s = ans
+        palindromes = set()
+        for i in range(len(s)):
+            palindromes = expandAroundCenter(s, i, i, palindromes)
+            palindromes = expandAroundCenter(s, i, i+1, palindromes)
 
-    palindromes = list(palindromes)
-    palindromes = sorted(palindromes, key=lambda x:len(x), reverse=True)
+        palindromes = list(palindromes)
+        palindromes = sorted(palindromes, key=lambda x:len(x), reverse=True)
 
-    if palindromes:
-        for i in range(26):
-            shift = 0
-            ori_string = ""
-            alpha = ord(palindromes[0][0]) - 97
-            diff = alpha - i
-            diff = diff if diff >= 0  else 26+diff
-            for char in palindromes[0]:
-                code = (ord(char) - 97 + diff) % 26 + 97
-                ori_string += chr(code)
-                shift += code
-            shift += len(palindromes)
-            
-            found = True
-            for j in range(len(ori_string)):
-                if chr((ord(ori_string[j]) - 97 + shift) % 26 + 97) != palindromes[0][j]:
-                    found = False
+        if palindromes:
+            for i in range(26):
+                shift = 0
+                ori_string = ""
+                alpha = ord(palindromes[0][0]) - 97
+                diff = alpha - i
+                diff = diff if diff >= 0  else 26+diff
+                for char in palindromes[0]:
+                    code = (ord(char) - 97 + diff) % 26 + 97
+                    ori_string += chr(code)
+                    shift += code
+                shift += len(palindromes)
+                
+                found = True
+                for j in range(len(ori_string)):
+                    if chr((ord(ori_string[j]) - 97 + shift) % 26 + 97) != palindromes[0][j]:
+                        found = False
+                        break
+
+                if found:
                     break
 
-            if found:
+            ans = ""
+
+            for char in s:
+                ans += chr((ord(char) - 97 + diff) % 26 + 97)
+
+        i = 0
+        real_ans = ""
+        word = False
+        for j in range(2, len(ans)):
+            if d.check(ans[:j]):
+                word = True
                 break
 
-        ans = ""
+        if not word:
+            real_ans = ""
+        else:
+            real_ans = ans
 
-        for char in s:
-            ans += chr((ord(char) - 97 + diff) % 26 + 97)
-
-    else:
-        ans = s
-
-    return ans
+    return real_ans, encryption_count
 
 def expandAroundCenter(s, l, r, palindromes):
     while l >= 0 and r < len(s) and s[l] == s[r]:
@@ -63,7 +81,8 @@ def evaluateBoredScribe():
     logging.info("data sent for evaluation {}".format(data))
     result = []
     for el in data:
-        result.append({"id": el.get("id"), "encryptionCount":el.get("id"), "originalText":decrypt(el.get("encryptedText"))})
+        originalText, encryptionCount = decrypt(el.get("encryptedText"))
+        result.append({"id": el.get("id"), "encryptionCount":encryptionCount, "originalText":originalText})
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
