050b489f38bf4adc99eeda40aafdb78c958e8d8a
kaishern <leek0125@e.ntu.edu.sg>
2020-09-26 23:21:44 +0800

    bug fix

--- codeitsuisse/routes/SLSM.py
+++ codeitsuisse/routes/SLSM.py
@@ -9,13 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 def slsm(boardSize, player, jumps):
-    board = [i for i in range(boardSize)]
-    ans = []
+    board = [i for i in range(boardSize + 1)]
 
     for jump in jumps:
         temp = jump.split(":")
         if int(temp[0]) == 0:
-            board[int(temp[1])] = min(int(temp[1]) + 6, boardSize-1)
+            board[int(temp[1])] = min(int(temp[1]) + 6, boardSize)
         elif int(temp[1]) == 0:
             board[int(temp[0])] = max(int(temp[0]) - 6, 0)
         elif int(temp[0]) > int(temp[1]):
@@ -23,21 +22,22 @@ def slsm(boardSize, player, jumps):
         elif int(temp[0]) < int(temp[1]):
             board[int(temp[1])] = int(temp[1])
 
-    x = 0
-    y = 0
+    # print(board[330:])
+    x = 1
+    y = 1
     shortest_path = []
 
     while x != boardSize:
         next_6 = board[x + 1:x + 6 + 1]
         if not next_6:
             break
-        if boardSize - 1 in next_6:
-            shortest_path.append(next_6.index(boardSize - 1) + 1)
+        if boardSize in next_6:
+            shortest_path.append(next_6.index(boardSize) + 1)
             break
 
         next_6_l = board[y + 1:y + 6 + 1]
-        best_choice_l= next_6_l.index(min(next_6_l)) + 1
-        for _ in range(player-1):
+        best_choice_l = next_6_l.index(min(next_6_l)) + 1
+        for _ in range(player - 1):
             shortest_path.append(best_choice_l)
         y = min(next_6_l)
 
@@ -45,8 +45,8 @@ def slsm(boardSize, player, jumps):
         shortest_path.append(best_choice)
         x = max(next_6)
 
+    return shortest_path
 
-    return ans
     # board = [0 for i in range(boardSize+1)]
     #
     # for jump in jumps:
