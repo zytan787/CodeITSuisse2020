716db9bb4fbf9d9e43bcf2ae1028e269ef5be022
zy tan <zarn@wright.com.sg>
2020-09-27 00:48:16 +0800

    Update magical fruit basket.py

--- codeitsuisse/routes/magical fruit basket.py
+++ codeitsuisse/routes/magical fruit basket.py
@@ -11,11 +11,22 @@ logger = logging.getLogger(__name__)
 def evaluateFruitBasket():
     data = request.get_data()
     logging.info("data sent for evaluation {}".format(data))
-    count = 0
+    weight = 0
     data = json.loads(data)
     for key in data:
-        count += data[key]
-    result = count * 0
+        if key == "maPomegranate":
+            weight += data[key] * 30
+        elif key == "maRamubutan":
+            weight += data[key] * 30
+        elif key == "maAvocado":
+            weight += data[key] * 10
+        elif key == "maWatermelon":
+            weight += data[key] * 60
+        elif key == "maApple":
+            weight += data[key] * 15
+        elif key == "maPineapple":
+            weight += data[key] * 80
+    result = weight
     logging.info("My result :{}".format(result))
     return jsonify(result)
 
