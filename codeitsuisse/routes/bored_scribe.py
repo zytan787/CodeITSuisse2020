e9a00eb5ffdeb9beffb77115dca238007011a93d
zy tan <zarn@wright.com.sg>
2020-09-27 11:35:41 +0800

    Update bored_scribe.py

--- codeitsuisse/routes/bored_scribe.py
+++ codeitsuisse/routes/bored_scribe.py
@@ -41,7 +41,7 @@ def decrypt(s):
         real_ans = ans
         return real_ans, encryption_count
 
-    while not real_ans and encryption_count < 10:
+    while not real_ans and encryption_count < 9:
         encryption_count += 1
         s = ans
         palindromes = set()
@@ -99,7 +99,7 @@ def decrypt(s):
             real_ans = ans
             return real_ans, encryption_count
 
-    if encryption_count == 10 and not real_ans:
+    if encryption_count == 9 and not real_ans:
         real_ans = ans
     return real_ans, encryption_count
 
